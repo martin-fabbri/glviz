b33c914b184bb20ad991d701269abd08c6cdb98e
Martin Fabbri <martin.fabbri.a@gmail.com>
2018-10-07 16:13:47 -0700

    Remove unused assets.

--- src/app.tsx
+++ src/app.tsx
@@ -1,15 +1,18 @@
 import * as React from 'react';
-import { BrowserRouter as Router, Link, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Model01 from './models/01-simple-rectangle';
+import Navigation from './navigation';
 
 class App extends React.Component {
     public render() {
         return (
             <Router>
                 <>
-                    <Link to='01-simple-rectangle'>01-simple-rectangle</Link>
-
-                    <Route path='01-simple-rectangle' component={Model01}/>
+                    <Navigation/>
+                    <Switch>
+                        <Route path='/' component={Model01} exact={true} />
+                        <Route component={Model01} />
+                    </Switch>
                 </>
             </Router>
         );
---
+++ src/navigation.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navigation = () => {
+    return (
+        <div>
+            <NavLink to={'/'}>01 Simple Rect</NavLink>
+        </div>
+    );
+};
+
+export default Navigation;
